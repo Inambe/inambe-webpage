552ebc96a89764a5d018d8f2f06090a869f83a18
Inam Ul Huq <inambe.dev@gmail.com>
2020-01-08 13:12:59 +0500

    set h1 font size + container width + add main in home

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,7 +2,9 @@ import React from "react";
 import Hero from "../components/hero";
 
 function Home() {
-	return <Hero />;
+    return <main>
+        <Hero />
+    </main>;
 }
 
 export default Home;
