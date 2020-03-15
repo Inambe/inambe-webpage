4327a4b6fb2c49728cbf0d2514bf7385c9ce423a
Inam Ul Huq <inambe.dev@gmail.com>
2020-03-15 19:45:32 +0500

    add Resume

--- src/components/a.js
+++ src/components/a.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./a.css";
 
-function A({ newTab, children, rrLink, ...props }) {
+function A({ newTab, children, rrLink, noStyle, ...props }) {
 	let customProps = {};
-	customProps.className = "a";
+	customProps.className = noStyle ? null : "a";
 	customProps.target = newTab ? "_blank" : undefined;
 	return rrLink ? (
 		<Link {...customProps} {...props}>
--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import "./header.css";
 
 import Container from "./container";
+import A from "./a";
+import Resume from "./../static/Inam-Ul-Huq-Resume.pdf";
 
 function Header() {
 	return (
@@ -12,6 +14,9 @@ function Header() {
 					<Link to="/">Home</Link>
 					<Link to="/projects">Projects</Link>
 					<Link to="/about">About</Link>
+					<A href={Resume} newTab noStyle className="fancy">
+						Resume
+					</A>
 					<Link to="/contact">Contact</Link>
 				</nav>
 			</Container>
