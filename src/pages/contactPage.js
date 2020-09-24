86a4004750f4696f4a59a32682210698df5fb1e2
Inam Ul Huq <inambe.dev@gmail.com>
2020-09-25 01:22:24 +0500

    fixed contact page

--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -1,101 +1,115 @@
-import React from "react";
+import React from "react"
 import {
 	FaGithub,
 	FaEnvelope,
 	FaPhone,
 	FaTwitter,
-	FaLinkedin
-} from "react-icons/fa";
-import { Row, Col } from "react-grid-system";
-import "./contactPage.css";
+	FaLinkedin,
+} from "react-icons/fa"
+import { Row, Col } from "react-grid-system"
+import "./contactPage.css"
 
-import Input from "../components/form/input";
-import FormControl from "../components/form/formControl";
-import Label from "../components/form/label";
-import Button from "../components/button";
-import A from "../components/a";
-import Container from "../components/container";
+import Input from "../components/form/input"
+import FormControl from "../components/form/formControl"
+import Label from "../components/form/label"
+import Button from "../components/button"
+import A from "../components/a"
+import Container from "../components/container"
+import LayoutRoot from "../layouts/root"
+import MainLayout from "../layouts/main"
 
 function ContactPage() {
 	return (
-		<main className="contactPage">
-			<Container>
-				<h1>Contact</h1>
-				<Row>
-					<Col md={5} className="contactPage__section">
-						<p>
-							If you have any question for me or just wanna say
-							hello, use the form below.
-						</p>
-						<form method="post" name="contact-form">
-							{/* for netlify */}
-							<input
-								type="hidden"
-								name="form-name"
-								value="contact-form"
-							/>
+		<LayoutRoot>
+			<MainLayout>
+				<main className="contactPage">
+					<Container>
+						<h1>Contact</h1>
+						<Row>
+							<Col md={5} className="contactPage__section">
+								<p>
+									If you have any question for me or just
+									wanna say hello, use the form below.
+								</p>
+								<form method="post" name="contact-form">
+									{/* for netlify */}
+									<input
+										type="hidden"
+										name="form-name"
+										value="contact-form"
+									/>
 
-							<FormControl>
-								<Label>Your name</Label>
-								<Input name="name" block />
-							</FormControl>
-							<FormControl>
-								<Label>Your email*</Label>
-								<Input
-									name="email"
-									type="email"
-									required
-									block
-								/>
-							</FormControl>
-							<FormControl>
-								<Label>Message*</Label>
-								<Input name="message" textarea required block />
-							</FormControl>
-							<FormControl>
-								<Button type="primary" size="m">
-									Send
-								</Button>
-							</FormControl>
-						</form>
-					</Col>
-					<Col
-						md={5}
-						offset={{ md: 2 }}
-						className="contactPage__section contactInfo"
-					>
-						<p>Some other ways you can contact me.</p>
-						<p className="contactInfo__item">
-							<FaEnvelope />{" "}
-							<A href="mailto:inambe.dev@gmail.com">
-								inambe.dev@gmail.com
-							</A>
-						</p>
-						<p className="contactInfo__item">
-							<FaPhone /> +92 340 4678646
-						</p>
-						<p className="socialIcons">
-							<A href="https://github.com/inambe" target="_blank">
-								<FaGithub />
-							</A>
-							<A
-								href="https://twitter.com/MeInamUlHuq"
-								target="_blank"
+									<FormControl>
+										<Label>Your name</Label>
+										<Input name="name" block />
+									</FormControl>
+									<FormControl>
+										<Label>Your email*</Label>
+										<Input
+											name="email"
+											type="email"
+											required
+											block
+										/>
+									</FormControl>
+									<FormControl>
+										<Label>Message*</Label>
+										<Input
+											name="message"
+											textarea
+											required
+											block
+										/>
+									</FormControl>
+									<FormControl>
+										<Button type="primary" size="m">
+											Send
+										</Button>
+									</FormControl>
+								</form>
+							</Col>
+							<Col
+								md={5}
+								offset={{ md: 2 }}
+								className="contactPage__section contactInfo"
 							>
-								<FaTwitter />
-							</A>
-							<A
-								href="https://www.linkedin.com/in/inambe"
-								target="_blank"
-							>
-								<FaLinkedin />
-							</A>
-						</p>
-					</Col>
-				</Row>
-			</Container>
-		</main>
-	);
+								<p>Some other ways you can contact me.</p>
+								<p className="contactInfo__item">
+									<FaEnvelope />{" "}
+									<A href="mailto:inambe.dev@gmail.com">
+										inambe.dev@gmail.com
+									</A>
+								</p>
+								<p className="contactInfo__item">
+									<FaPhone /> +92 340 4678646
+								</p>
+								<p className="socialIcons">
+									<A
+										href="https://github.com/inambe"
+										target="_blank"
+									>
+										<FaGithub />
+									</A>
+									<A
+										href="https://twitter.com/MeInamUlHuq"
+										target="_blank"
+									>
+										<FaTwitter />
+									</A>
+									<A
+										href="https://www.linkedin.com/in/inambe"
+										target="_blank"
+									>
+										<FaLinkedin />
+									</A>
+								</p>
+							</Col>
+						</Row>
+					</Container>
+				</main>
+			</MainLayout>
+		</LayoutRoot>
+	)
 }
 
-export default ContactPage;
+export default ContactPage
