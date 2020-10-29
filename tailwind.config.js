fe6b168a14cf9902088f25506a91d8d8b72cc46a
Inam Ul Huq <inambe.dev@gmail.com>
2020-10-30 00:52:59 +0500

    hero converted to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+	future: {
+		// removeDeprecatedGapUtilities: true,
+		// purgeLayersByDefault: true,
+	},
+	purge: [
+		"./src/**/*.js",
+		"./src/**/*.jsx",
+		"./src/**/*.ts",
+		"./src/**/*.tsx",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#2872ff",
+			},
+			borderWidth: {
+				"half-rem": "0.5rem",
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
 }
