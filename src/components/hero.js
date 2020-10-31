1cdc57665002a36cb7080447302728bb536ba125
Inam Ul Huq <inambe.dev@gmail.com>
2020-10-31 23:17:12 +0500

    fix hero sizing

--- src/components/hero.js
+++ src/components/hero.js
@@ -21,13 +21,13 @@ function Hero() {
 	const portrait = data.file.childImageSharp.fluid
 
 	return (
-		<div className="flex items-center flex-col-reverse -mx-2 md:flex-row">
-			<div className="w-full mb-6 mx-2 text-center md:w-4/6 md:text-left">
+		<div className="flex items-center flex-col-reverse md:flex-row md:-mx-2">
+			<div className="w-full mb-6 text-center md:text-left md:mx-2 md:w-8/12 lg:w-6/12">
 				<div className="mb-8">
 					<h1 className="text-primary font-bold text-6xl leading-none">
 						Inam Ul Huq
 					</h1>
-					<h3 className="text-1xl italic">Software Developer</h3>
+					<h3 className="text-xl italic">Software Developer</h3>
 				</div>
 				<div className="mb-8">
 					<p className="mb-4 text-lg">
@@ -52,7 +52,7 @@ function Hero() {
 					</Button>
 				</A>
 			</div>
-			<div className="w-2/3 mb-6 mx-2 md:w-2/6">
+			<div className="w-2/3 mb-6 md:mx-2 md:w-4/12 lg:w-4/12 lg:mx-auto">
 				<Image
 					fluid={portrait}
 					alt="Inam Ul Huq"
