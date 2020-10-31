f59c4400a901e93f7a77b12419a8ad271e9d45f4
Inam Ul Huq <inambe.dev@gmail.com>
2020-10-31 17:21:08 +0500

    imp. about using tailwind

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,47 @@
 import React from "react"
-import { Row, Col } from "react-grid-system"
-import "./aboutPage.css"
+import { graphql, useStaticQuery } from "gatsby"
+import Image from "gatsby-image"
 
-import Portrait from "./../static/media/imgs/inam-ul-huq.png"
-
-import Img from "../components/img"
 import SkillIcons from "../components/skillIcons"
 import MainLayout from "../layouts/main"
 import LayoutRoot from "../layouts/root"
 import SEO from "../components/seo"
 
+const Section = props => {
+	return (
+		<div
+			className="flex items-center flex-col -mx-2 mb-8 md:flex-row md:mb-16"
+			{...props}
+		/>
+	)
+}
+const SectionHeading = props => {
+	// eslint-disable-next-line jsx-a11y/heading-has-content
+	return <h2 className="text-4xl text-primary font-extrabold" {...props} />
+}
+
 function AboutPage() {
+	const data = useStaticQuery(graphql`
+		{
+			file(relativePath: { eq: "inam-ul-huq.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 500) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+	`)
+	const portrait = data.file.childImageSharp.fluid
 	return (
 		<LayoutRoot>
-			<SEO title="About" description="Read more about me." />
+			<SEO title="About" description="Read more about Inam Ul Huq." />
 			<MainLayout>
-				<main className="aboutPage">
-					<Row align="center" className="aboutPage-sec about-sec">
-						<Col>
-							<p>
+				<main>
+					<Section>
+						<div className="w-full mx-2 md:w-9/12">
+							<SectionHeading>About</SectionHeading>
+							<p className="mb-2">
 								Hi{" "}
 								<span role="img" aria-label="Hi">
 									👋
@@ -26,37 +49,37 @@ function AboutPage() {
 								,<br /> I'm Inam Ul Huq, a Software Developer. I
 								build Web, Desktop, and Mobile applications.
 							</p>
-							<p>
+							<p className="mb-2">
 								I learned software development on my own from
 								online courses, YouTube, and documents including
 								books, blogs, and software documentations.
 							</p>
-							<p>
+							<p className="mb-2">
 								I'd done Diploma of Associate Engineering (DAE)
 								in Electronics from GCT Sialkot, Pakistan. While
 								I was in college, I learned about programming
 								Microcontrollers which led me to the field of
 								computer software.
 							</p>
-						</Col>
-						<Col md={4}>
-							<Img src={Portrait} alt="Inam Ul Huq" />
-						</Col>
-					</Row>
-					<Row align="center" className="aboutPage-sec">
-						<Col md={5}>
-							<h2 className="aboutPage-title">Skills</h2>
-							<p>
+						</div>
+						<div className="w-full mx-2 md:w-3/12">
+							<Image fluid={portrait} alt="Inam Ul Huq" />
+						</div>
+					</Section>
+					<Section>
+						<div className="w-full mx-2 md:w-1/3">
+							<SectionHeading>Skills</SectionHeading>
+							<p className="mb-2">
 								I had tried several different
 								technologies/languages including <u>Java</u>,{" "}
 								<u>C#</u>, and <u>C</u> but I'm profound and
 								professionally experienced in the listed ones.
 							</p>
-						</Col>
-						<Col md={7}>
+						</div>
+						<div className="w-full mx-2 md:w-2/3">
 							<SkillIcons />
-						</Col>
-					</Row>
+						</div>
+					</Section>
 				</main>
 			</MainLayout>
 		</LayoutRoot>
