6d6417ed3684d2449c66c16848eeab90d3f206ba
Inam Ul Huq <inambe.dev@gmail.com>
2020-11-01 22:31:22 +0500

    use tailwind space and grid

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -5,23 +5,21 @@ import Img from "./img"
 import A from "./a"
 
 const CardLink = props => {
-	return <A eLink newTab className="p-3 no-underline" {...props} />
+	return <A eLink newTab className="p-2 no-underline" {...props} />
 }
 
 function ProjectCard({ project }) {
 	const { frontmatter } = project
 
 	return (
-		<div className="border border-gray-500 bg-white">
+		<div className="border bg-white">
 			<div>
 				<Img src={frontmatter.thumbnail} />
 			</div>
-			<div className="text-center px-2">
-				<h3 className="text-xl font-semibold leading-none my-2">
-					{frontmatter.title}
-				</h3>
-				<p className="leading-none mb-2">{frontmatter.description}</p>
-				<div className="text-xs mb-2">
+			<div className="text-center border-b p-2 space-y-2">
+				<h3 className="text-xl font-semibold">{frontmatter.title}</h3>
+				<p className="text-sm">{frontmatter.description}</p>
+				<div className="text-xs">
 					{frontmatter.uses.map((u, i) => (
 						<span
 							className="inline-block bg-gray-300 mr-1 py-1 px-2 rounded"
