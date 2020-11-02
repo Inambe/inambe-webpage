56656a38e50f16f9f66ecf7e79818f803f0d7ef5
Inam Ul Huq <inambe.dev@gmail.com>
2020-11-02 20:46:10 +0500

    increase main container side padding

--- src/components/container.js
+++ src/components/container.js
@@ -2,9 +2,9 @@ import React from "react"
 
 function Container({ main, ...props }) {
 	return main ? (
-		<main className="container mx-auto px-3 py-12" {...props} />
+		<main className="container mx-auto px-5 py-12" {...props} />
 	) : (
-		<div className="container mx-auto px-3" {...props} />
+		<div className="container mx-auto px-5" {...props} />
 	)
 }
 
