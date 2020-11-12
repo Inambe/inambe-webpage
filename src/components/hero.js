cc072407d6f39e1e221e87197a759e66aa15435a
Inam Ul Huq <inambe.dev@gmail.com>
2020-11-12 16:21:53 +0500

    optz hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -21,7 +21,7 @@ function Hero() {
 	const portrait = data.file.childImageSharp.fluid
 
 	return (
-		<div className="min-h-full-in-container flex items-center flex-col-reverse md:flex-row md:-mx-2">
+		<div className="min-h-full-in-container flex items-center flex-col-reverse md:flex-row md:-mx-2 md:justify-between">
 			<div className="w-full mb-6 text-center md:text-left md:mx-2 md:w-8/12 lg:w-6/12">
 				<div className="mb-8">
 					<h1 className="text-primary text-4xl font-bold md:text-6xl leading-none">
@@ -52,7 +52,7 @@ function Hero() {
 					</Button>
 				</A>
 			</div>
-			<div className="w-2/3 mb-6 md:mx-2 md:w-4/12 lg:w-4/12 lg:mx-auto">
+			<div className="w-2/3 mb-6 md:mx-2 md:w-4/12">
 				<Image
 					fluid={portrait}
 					alt="Inam Ul Huq"
